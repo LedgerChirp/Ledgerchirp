a21080977b44482ef5532bfc69d965ec9955cfe8
Mohit-Aasirwal <mohitaasirwal@gmail.com>
2023-10-29 16:40:19 +0530

    hire section fixed

--- components/Hire/Hire.tsx
+++ components/Hire/Hire.tsx
@@ -13,7 +13,7 @@ const Hire = () => {
         src={"/glare.svg"}
         width={100}
         height={100}
-        className="md:w-40 md:h-40 w-20 h-20 absolute md:right-[6rem] md:left-0 left-[35%] right-0"
+        className="md:w-40 md:h-40 w-20 h-20 absolute md:right-[6rem] right-[7rem]"
         alt="glare"
       />
       <button className="ml-[4.5rem] mt-6 md:ml-0 md:mt-0 bg-gradient-to-tr from-[#F16063] to-[#FFC656] rounded-lg text-xs md:text-base py-2 px-4 md:py-3 md:px-5">
